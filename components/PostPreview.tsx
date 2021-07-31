22ff07339d1cc6573f3b43cfb8e8a366bdaa9232
Daniel Olavio <danielolaviobr@gmail.com>
2021-07-31 09:26:11 +0100

    blog listing page

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,46 +3,49 @@ import Image from "next/image";
 import React from "react";
 
 interface PostProps {
-  title?: string;
-  description?: string;
-  image?: { path: string | StaticImageData; alt: string };
-  url?: string;
+	title?: string;
+	description?: string;
+	date?: string;
+	image?: { path: string | StaticImageData; alt: string };
+	url?: string;
 }
 
 export default function PostPreview({
-  title = "Lorem Ipsum dolor sit ame",
-  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac fringilla magna. Etiam ultricies sem velit, eu sodales eros faucibus nec. Donec volutpat orci augue. Nunc dui dolor, pharetra ut volutpat id, consequat feugiat massa.",
-  image = { path: "/blog.webp", alt: "Next.js pagination" },
-  url = "#",
+	title = "Lorem Ipsum dolor sit ame",
+	date = "01/01/2021",
+	description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac fringilla magna. Etiam ultricies sem velit, eu sodales eros faucibus nec. Donec volutpat orci augue. Nunc dui dolor, pharetra ut volutpat id, consequat feugiat massa.",
+	image = { path: "/blog.webp", alt: "Next.js pagination" },
+	url = "#",
 }: PostProps) {
-  return (
-    <div className="flex flex-col my-8 lg:flex-row">
-      <Link href={url} aria-label={title}>
-        <a>
-          <Image
-            src={image.path}
-            width={360}
-            height={240}
-            objectFit="cover"
-            alt={image.alt}
-            className="rounded-md"
-            placeholder={typeof image.path === "string" ? "empty" : "blur"}
-          />
-        </a>
-      </Link>
-      <div className="flex flex-col max-w-sm mt-8 lg:ml-12 lg:mt-0">
-        <h3 className="mb-4 text-lg font-medium">
-          <Link href={url} aria-label={title}>
-            <a>{title}</a>
-          </Link>
-        </h3>
-        <p className="line-clamp-4">{description}</p>
-        <span className="mt-auto mb-4 font-medium hover:underline">
-          <Link href={url} aria-label={title}>
-            <a>Read more</a>
-          </Link>
-        </span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col my-8 lg:flex-row">
+			<Link href={url} aria-label={title}>
+				<a>
+					<Image
+						src={image.path}
+						width={360}
+						height={240}
+						objectFit="cover"
+						alt={image.alt}
+						className="rounded-md"
+						placeholder={typeof image.path === "string" ? "empty" : "blur"}
+					/>
+				</a>
+			</Link>
+			<div className="flex flex-col max-w-sm mt-8 lg:ml-12 lg:mt-0">
+				<h3 className="mb-2 text-lg font-medium">
+					<Link href={url} aria-label={title}>
+						<a>{title}</a>
+					</Link>
+				</h3>
+				<span className="mb-4 text-sm text-gray-400">{date}</span>
+				<p className="line-clamp-4">{description}</p>
+				<span className="mt-auto mb-4 font-medium hover:underline">
+					<Link href={url} aria-label={title}>
+						<a>Read more</a>
+					</Link>
+				</span>
+			</div>
+		</div>
+	);
 }
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { GetStaticProps } from "next";
+import { getAllPostsMeta } from "utils/mdx";
+import PostPreview from "components/PostPreview";
+import { PostMeta } from "types/Post";
+import { format } from "date-fns";
 
-export default function Blog() {
-  return <div>Blog listing page</div>;
+interface BlogProps {
+	posts: PostMeta[];
 }
+
+export default function Blog({ posts }: BlogProps) {
+	return (
+		<div>
+			{posts.map((post) => (
+				<PostPreview
+					key={post.slug}
+					title={post.title}
+					date={post.publishedAt}
+					description={post.description}
+					image={{ path: post.image || "", alt: post.alt as string }}
+				/>
+			))}
+		</div>
+	);
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const posts = getAllPostsMeta("post");
+	posts.forEach((post) => {
+		post.publishedAt = format(post.publishedAt, "dd/MM/yyyy");
+	});
+	//console.log(posts);
+
+	return {
+		props: { posts },
+	};
+};
